25ea09356835aa820777cb671d519dfd104fa495
Administrator <Administrator@n-9c4c592af9fe4>
2015-10-15 20:55:07 -0400

    progressss with Assignment 4

--- src/Question5.java
+++ src/Question5.java
@@ -19,20 +19,53 @@ public class Question5 {
         // TODO code application logic here
         Scanner in = new Scanner(System.in);
         
-        String myString;
-        int myInt;
-        int myInt2;
-        int myInt3;
-        int myInt4;
-        int myInt5;
-        int myInt6;
-        int myInt7;
-        int myInt8;
-        int myInt9;
-        int myInt10;
-        int myInt11;
+        String name;
+        int test1;
+        int testr1;
+        int test2;
+        int testr2;
+        int test3;
+        int testr3;
+        int test4;
+        int testr4;
+        int test5;
+        int testr5;
+        int result;
         
         System.out.println("Please enter your name");
+         name = in.next();
+         
+         System.out.println("What was the first test out of?");
+        test1 = in.nextInt();
         
+        System.out.println("What mark did you get?");
+        testr1 = in.nextInt();
+        
+        System.out.println("What was the second test out of?");
+        test2 = in.nextInt();
+        
+        System.out.println("What mark did you get?");
+        testr2 = in.nextInt();
+        
+        System.out.println("What was the third test out of?");
+        test3 = in.nextInt();
+        
+        System.out.println("What mark did you get?");
+        testr3 = in.nextInt();
+        
+        System.out.println("What was the fourth test out of?");
+        test4 = in.nextInt();
+        
+        System.out.println("What mark did you get?");
+        testr4 = in.nextInt();
+        
+        System.out.println("What was the fifth test out of?");
+        test5 = in.nextInt();
+        
+        System.out.println("What mark did you get?");
+        testr5 = in.nextInt();
+        
+        System.out.println("Test Scores for" + name + "Test 1" + (test1/testr1*100) + "% Test 2" +(test2));
+       
     }
 }
