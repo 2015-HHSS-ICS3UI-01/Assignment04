5b896f4e080dc6ed1e67a1be946750acf6bbe121
Sami <Sami@Macintosh>
2015-10-18 14:41:30 -0400

    First Assignment

---
+++ src/Question06.java
@@ -0,0 +1,67 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Sami
+ */
+public class Question06 {
+
+    public static void main(String[] args) {
+
+        //make a scanner
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("See which plan is best for you!");
+
+        //asks user the number of daytime, evening and weekend minutes
+        System.out.print("Enter the number of daytime minutes: ");
+        int daytimeMinutes = input.nextInt();
+
+        System.out.print("Enter the number of evening minutes: ");
+        int eveningMinutes = input.nextInt();
+
+        System.out.print("Enter the number of weekend minutes: ");
+        int weekendMinutes = input.nextInt();
+
+        //calculates how many minutes user has gone over on plan A
+        double daytimeA = 100;
+        double totalMinutesA = daytimeMinutes - daytimeA;
+        double daytimeTotalA = totalMinutesA * 0.25;
+
+        //calculates how many minutes user has gone over on plan B
+        double daytimeB = 250;
+        double totalMinutesB = daytimeMinutes - daytimeB;
+        double daytimeTotalB = totalMinutesB * 0.45;
+
+        //calculates amount to pay on evening minutes
+        double eveningA = eveningMinutes * 0.15;
+        double eveningB = eveningMinutes * 0.35;
+
+        //calculates amount to pay on weekend minutes
+        double weekendA = weekendMinutes * 0.20;
+        double weekendB = weekendMinutes * 0.25;
+
+        //add up the total for plan A and plan B
+        double planA = daytimeTotalA + eveningA + weekendA;
+        double planB = daytimeTotalB + eveningB + weekendB;
+
+        //shows user the prices of plan A and plan B
+        System.out.println("Plan A costs: $" + planA);
+        System.out.println("Plan B costs: $" + planB);
+
+        //tells user which plan would be cheaper
+        if (planA < planB) {
+            System.out.println("Plan A is the cheapest.");
+        } else if (planA > planB) {
+            System.out.println("Plan B is the cheapest.");
+        } else {
+            System.out.println("Both plans cost the same.");
+        }
+    }
+}
---
+++ src/Question07.java
@@ -0,0 +1,55 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Sami
+ */
+public class Question07 {
+
+    public static void main(String[] args) {
+
+        //make a scanner
+        Scanner input = new Scanner(System.in);
+
+        //user enters speed limit
+        System.out.print("Enter the speed limit: ");
+        int speedLimit = input.nextInt();
+
+        //user enters speed of car
+        System.out.print("Enter the recorded speed of the car: ");
+        int carSpeed = input.nextInt();
+
+        //three fines
+        int firstFine = 100;
+        int secondFine = 270;
+        int thirdFine = 500;
+
+        //checks how much they are speeding by
+        int speedOver = carSpeed - speedLimit;
+
+        //check if under speed limit by 15km/h
+        if (speedOver < -15) {
+            System.out.println("Speed up, you might be causing traffic!");
+        } //check if in the speed limit
+        else if (carSpeed < speedLimit && speedOver < -14) {
+            System.out.println("Congratulations, you are within the speed limit!");
+        } //checks if cars speed is more than speed limit
+        else if (carSpeed > speedLimit) {
+
+            //checks all 3 possibilites 
+            if (speedOver > 1 && speedOver < 21) {
+                System.out.println("You are speeding over by " + speedOver + "km/h, your fine is $" + firstFine + ".");
+            } else if (speedOver > 21 && speedOver < 30) {
+                System.out.println("You are speeding over by " + speedOver + "km/h, your fine is $" + secondFine + ".");
+            } else if (speedOver > 31) {
+                System.out.println("You are speeding over by " + speedOver + "km/h, your fine is $" + thirdFine + ".");
+            }
+        }
+    }
+}
