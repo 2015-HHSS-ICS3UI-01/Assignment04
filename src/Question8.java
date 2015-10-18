67e7f50b0ddff9bd4cf08fa8493cd9c859d0e820
Hadida <Hadida@Hadida-PC>
2015-10-18 13:43:20 -0400

    Progress

---
+++ src/Question8.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hadida
+ */
+public class Question8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        
+        while(true){
+            System.out.println("Enter sum of dice ");
+            int sum = in.nextInt();
+            
+            System.out.println("You are now on square " + sum + );
+            
+            
+        }
+    }
+    
+}
