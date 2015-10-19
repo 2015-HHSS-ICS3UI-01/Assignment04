73c615b167edb90c2f681ee06b602fa9afa5bd46
hadik9595 <hadik9595@HRH-IC0027323.wrdsb.ca>
2015-10-19 13:19:09 -0400

    Progress

--- src/Question6.java
+++ src/Question6.java
@@ -16,9 +16,9 @@ public class Question6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Generates a scanner
         Scanner in = new Scanner(System.in);
-        
+        //Asks the human various questions
         System.out.println("Number of daytime minutes?");
         int daytime = in.nextInt();
         
@@ -27,25 +27,30 @@ public class Question6 {
         
         System.out.println("Number of weekend minutes?");
         int weekend = in.nextInt();
-        
+        double planA = 0;
+        double planB = 0;
+        // If they used less than 100 daytime minutes for  planA
             if(daytime <100){
-                System.out.println("Plan A costs " +(evening*0.15) + (weekend*0.20));
+                planA = ((evening*0.15) + (weekend*0.20));
                 
             }else{
-                System.out.println("");
-                double planA = ((daytime-100)*0.25 + (evening*0.15) + (weekend*0.20));
+                // If daytime minutes are more than 100
+                planA = ((daytime-100)*0.25 + (evening*0.15) + (weekend*0.20));
+                //If they used less than 250 daytime minutes for planB
             }if(daytime < 250){
-                System.out.println("Plan B costs " + (evening*0.35) + (weekend*0.25));
+                planB= ((evening*0.35) + (weekend*0.25));
+                // If they used more than 250 daytime minutes for planB
             }else{
-        double planB = ((daytime-250)*0.45 + (evening*0.35) + (weekend*0.25));
-            
+                planB = ((daytime-250)*0.45 + (evening*0.35) + (weekend*0.25));
+                
+            }    
         
         System.out.println("Plan A costs "+ planA);
         System.out.println("Plan B costs " + planB);
-        
+        planB = Math.round(planB*100)/100.0;
             
         
-        
+        // This is analyzes comparison between both plans
         if(planA > planB){
             System.out.println("Plan B is cheapest.");
         }else if(planA < planB){
