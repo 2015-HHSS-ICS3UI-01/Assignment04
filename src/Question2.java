7d85ad9c391a7dfb0dd365e2d12f98a91d48e237
Matt <Matt@AllegaeonV2>
2015-10-19 12:38:09 -0400

    created q2, completed

--- src/Question2.java
+++ src/Question2.java
@@ -18,13 +18,14 @@ public class Question2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //new scanner for input
         Scanner input = new Scanner (System.in);
-        
+        //get measurement
         System.out.println("Please enter a measurement in inches");
         
         double measurement = input.nextDouble();
-        
-        System.out.println("The measurement is " + measurement*2.54);
+        //multiply measurement by 2.54 to get cm
+        System.out.println("The measurement is " + measurement*2.54 + "cm");
     }
     
 }
