6fe3424e57d2ca9693da5d581ae0d509cb7389bf
Matt <Matt@AllegaeonV2>
2015-10-19 12:39:10 -0400

    completed q3

--- src/Question3.java
+++ src/Question3.java
@@ -16,15 +16,16 @@ public class Question3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //scanner for input
         Scanner input = new Scanner(System.in);
-        
+        //get numbers
         System.out.println("Please enter 4 numbers on separate lines");
-        
+        //four separate lines
         double numberOne = input.nextDouble();
         double numberTwo = input.nextDouble();
         double numberThree = input.nextDouble();
         double numberFour = input.nextDouble();
-        
+        //print numbers out
         System.out.println("Your numbers are " + numberOne + "," + numberTwo + "," + numberThree + "," + numberFour);
 
     }
