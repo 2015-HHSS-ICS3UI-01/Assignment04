c435de3b92de6dfb4e070cc6fe8858fb589a6224
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2015-10-19 12:40:52 -0400

    Quiz3

--- src/A4Q5.java
+++ src/A4Q5.java
@@ -47,13 +47,45 @@ public class A4Q5 {
             //determine the percentage
             double p2 = a2*100/t2;
             
-        //test 3 %
             
-        
+        //test 3 %
+            //ask user the test total marks
+            System.out.println("What was the third test out of?");
+            //make variable for the test total marks
+            int t3 = input.nextInt ();
+            //ask user for actual mark
+            System.out.println("What did you get?");
+            //make variable for the actual mark
+            int a3 = input.nextInt();
+            //determine the percentage
+            double p3 = a3*100/t3;
+            
+        //test 4 %
+            //ask user the test total marks
+            System.out.println("What was the third test out of?");
+            //make variable for the test total marks
+            int t4 = input.nextInt ();
+            //ask user for actual mark
+            System.out.println("What did you get?");
+            //make variable for the actual mark
+            int a4 = input.nextInt();
+            //determine the percentage
+            double p4 = a3*100/t4;
+            
+        //tell user test scores in percent
         System.out.println("Test scores for " + name);
         System.out.println("Test 1: " + p1);
         System.out.println("Test 2: " + p2);
         System.out.println("Test 3: " + p3);
         System.out.println("Test 4: " + p4);
+        
+        //determine average       
+        double av = (t1+t2+t3+t4)/4;
+        
+        //tell user average
+        System.out.println("Your average is" + av);
+        
+        //close scanner
+        input.close();
     }
 }
