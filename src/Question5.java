b4be7138470e5ce40fb67f3c660c90dd4d6d0b91
Matt <Matt@AllegaeonV2>
2015-10-19 12:41:53 -0400

    completed q5

--- src/Question5.java
+++ src/Question5.java
@@ -17,12 +17,14 @@ public class Question5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //scanner for input
         Scanner input = new Scanner (System.in);
-        
+        //get student name
         System.out.println("Please enter your name, student.");
         
         String name = input.nextLine();
         System.out.println("Hello " + name + ", let's find your average mark.");
+        //get test "out of"s 1-5
         System.out.println("What was your first test out of?");
         
         double test1total = input.nextDouble();
@@ -44,6 +46,7 @@ public class Question5 {
         double test5total = input.nextDouble();
         
         System.out.println("Thank you " + name + ".");
+        //get marks 1-5
         System.out.println("What was your mark on your first test?");
         
         double test1mark = input.nextDouble();
@@ -65,15 +68,28 @@ public class Question5 {
         double test5mark = input.nextDouble();
         
         System.out.println("Thank you " + name + ".");
-        
+        //add all test "out of"s up
         double allTests = test1total+test2total+test3total+test4total+test5total;
-        
+        //add all marks up
         double allMarks = test1mark+test2mark+test3mark+test4mark+test5mark;
-        
+        //determine total
         double averageDecimal = allMarks/allTests;
-        
+        //determine mark percentage
         double averagePercent = averageDecimal*100;
-        
+        //determine individual marks
+        double test1percent = (test1mark/test1total)*100;
+        double test2percent = (test2mark/test2total)*100;
+        double test3percent = (test3mark/test3total)*100;
+        double test4percent = (test4mark/test4total)*100;
+        double test5percent = (test5mark/test5total)*100;
+        //output individual marks
+        System.out.println("Here are your individual test marks...");
+        System.out.println(test1percent);
+        System.out.println(test2percent);
+        System.out.println(test3percent);
+        System.out.println(test4percent);
+        System.out.println(test5percent);
+        //output total mark percentage
         System.out.println("Your approximate average mark is " + averagePercent + "%");
         
         
