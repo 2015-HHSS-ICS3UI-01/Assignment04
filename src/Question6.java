c6e01d7f2ef4d1909f734a695d8cacdb1f3312a4
dawsr2694 <dawsr2694@HRH-IC0027330.wrdsb.ca>
2015-10-21 13:22:15 -0400

    Question6-Completed

---
+++ src/Question6.java
@@ -0,0 +1,70 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author dawsr2694
+ */
+public class Question6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Create Scanner
+        Scanner input = new Scanner(System.in);
+
+        //Get input from user
+        //For daytime minutes
+        System.out.print("Number of daytime minutes?: ");
+        int day = input.nextInt();
+
+        //For evening minutes
+        System.out.print("Number of evening minutes?: ");
+        int night = input.nextInt();
+
+        //For weekend mintues
+        System.out.print("Number of weekend minutes?: ");
+        int week = input.nextInt();
+
+        //Figure out cost of plan A
+        //If number of minutes is less than total, set number of mins to 100
+        if (day < 100) {
+            day = 100;
+        }
+        double PlanA = (day - 100) * 0.25 + night * 0.15 + week * 0.20;
+
+        //Show user results for plan A
+        System.out.println("Plan A costs: " + PlanA);
+
+        //Round total to two decimal places
+        PlanA = Math.round(PlanA * 100) / 100.0;
+
+        //Figure out cost of Plan B
+        //If number of minutes is less than total, set number of mins to 250
+        if (day < 250) {
+            day = 250;
+        }
+        double PlanB = (day - 250) * 0.45 + night * 0.35 + week * 0.25;
+
+        //Round total to two decimal places
+        PlanB = Math.round(PlanB * 250) / 250.0;
+
+        //show user results for Plan B
+        System.out.println("Plan B costs: " + PlanB);
+
+        //Calculate cheapest option
+        if (PlanA < PlanB) {
+            System.out.println("Plan A is the cheapest.");
+        } else {
+            System.out.println("Plan B is the cheapest.");
+        }
+        if (PlanA == PlanB) {
+            System.out.println("Plan A and B cost the same.");
+        }
+    }
+}
