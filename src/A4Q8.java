9bfcd30e188e588f359e5c95c6832354b4736a1f
watsk8668 <watsk8668@HRH-IC0027333.wrdsb.ca>
2015-10-22 13:56:29 -0400

    A4 Q1,Q2,Q3,Q4,Q5,Q7,Q8 Done

--- src/A4Q8.java
+++ src/A4Q8.java
@@ -21,18 +21,49 @@ public class A4Q8 {
         //where is the game peice
         int place = 1;
 
-        while (place != 100){
-        //roll dice
-        System.out.println("Enter sum of dice: ");
-        int roll = input.nextInt();
-        
-        //move peice
-        int moveAmount = place + roll;
-        System.out.println("You are now on square  " + moveAmount);
-        } 
-        if(place == 100){
-            System.out.println("You win!");
+
+        while (true) {
+            if (place != 100) //roll dice
+            {
+                System.out.println("Enter sum of dice: ");
+            }
+            int roll = input.nextInt();
+            
+            //force a number between 2 and 12
+            if(roll < 2 || roll > 12) {
+                System.out.println("You Quit!");
+                break;
+            }
+            //snakes
+            if (place + roll == 54) {
+                place = 19;
+            }
+            if (place + roll == 90) {
+                place = 48;
+            }
+            if (place + roll == 99) {
+                place = 77;
+            }
+            //ladders
+            if (place + roll == 9) {
+                place = 34;
+            }
+            if (place + roll == 40) {
+                place = 64;
+            }
+            if (place + roll == 67) {
+                place = 86;
+            }
+            //move peice
+            int moveAmount = place + roll;
+            System.out.println("You are now on square " + moveAmount);
+            place = place + roll;
+            if (moveAmount == 100) {
+                System.out.println("You win!");
+                break;
+            }
         }
+
 
 
 
