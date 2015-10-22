718c9ca9655a066fbc3a4186b2feab2863eddb41
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2015-10-22 13:45:18 -0400

    l

---
+++ src/A4Q9.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sevcm7279
+ */
+public class A4Q9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // make a scanner
+        Scanner input = new Scanner (System.in);
+        
+        //make variable for player 1 starting at 1
+        int p1 = 1;
+        
+        //make variable for player 2 starting at 1
+        int p2 = 1;
+        
+       while (p1 != 100 && p2 != 100){
+           System.out.println("Enter sum of dice. ");
+            int sum = input.nextInt();
+                }
+    }
+}
