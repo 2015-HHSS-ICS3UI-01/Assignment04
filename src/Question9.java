f94c4c15e5ae19756c3dc2686d32ab79f35aa848
mitrm7692 <mitrm7692@HRH-IC0027327.wrdsb.ca>
2015-10-23 12:47:47 -0400

    Question9

--- src/Question9.java
+++ src/Question9.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author mitrm7692
@@ -18,14 +17,25 @@ public class Question9 {
     public static void main(String[] args) {
         // Create a new Scanner
         Scanner input = new Scanner(System.in);
+        //when game starts current space is 1 for player 1
+        int currentSpace1 = 1;
+        //when game starts current space is 1 for player 2
+        int currentSpace2 = 1;
         //when game starts current space is 1
         int currentSpace = 1;
         //Starting player is player 1
-        int currentPlayer=1;
+        int currentPlayer = 1;
         //loop until game is won or player quits
         while (true) {
-            //asks to enter sum of 2 dice
-            System.out.println("enter sum of dice");
+            //if current player is player 1 currentSpace1 is currentSpace
+            if (currentPlayer == 1) {
+                currentSpace = currentSpace1;
+            } else {
+                //if current player is player 2 currentSpace2 is currentSpace
+                currentSpace = currentSpace2;
+            }//asks to current player to enter sum of 2 dice
+            
+            System.out.println("Player " + currentPlayer + " enter sum of dice");
             //integer for sum of 2 dice
             int roll = input.nextInt();
             //if player types 0 it means they quit and game is over
@@ -48,53 +58,48 @@ public class Question9 {
             if (currentSpace == 9) {
                 currentSpace = 34;
                 System.out.println("You have gone up a ladder");
-            } 
-            //ladder on square 40 takes you to square 64
+            } //ladder on square 40 takes you to square 64
             else if (currentSpace == 40) {
                 currentSpace = 64;
                 System.out.println("You have gone up a ladder");
-            } 
-            //ladder on square 67 takes you to square 86
+            } //ladder on square 67 takes you to square 86
             else if (currentSpace == 67) {
                 currentSpace = 86;
                 System.out.println("You have gone up a ladder");
-            } 
-            //snake on square 54 takes you to square 19
+            } //snake on square 54 takes you to square 19
             else if (currentSpace == 54) {
                 currentSpace = 19;
                 System.out.println("You have landed on a snake");
-            } 
-            //snake on square 90 takes you to square 48
+            } //snake on square 90 takes you to square 48
             else if (currentSpace == 90) {
                 currentSpace = 48;
                 System.out.println("You have landed on a snake");
-            } 
-            //snake on square 99 takes you to square 77
+            } //snake on square 99 takes you to square 77
             else if (currentSpace == 99) {
                 currentSpace = 77;
                 System.out.println("You have landed on a snake");
             }
             //Scanner will output what square you are currently on
-            System.out.println("You are now on square " + currentSpace);
+            System.out.println("Player " + currentPlayer + " you are on " + currentSpace);
             //If you land on square 100 scanner says you win the game
             if (currentSpace == 100) {
                 System.out.println("You Win!");
                 //finish with program
                 break;
             }
-            while(true){
-                //if current player is player 1 switch to player 2
-                if (currentPlayer==1){
-                    currentPlayer=2;
-                    //stop loop
-                    break;
-                }
-                //if current player is player 2 switch to player 1
-                if (currentPlayer==2){
-                    currentPlayer=1;
-                    //stop loop
-                    break;
-                }
+
+            //if current player is player 1 switch to player 2 after player 1 reaches new square
+            if (currentPlayer == 1) {
+                currentPlayer = 2;
+                currentSpace1 = currentSpace;
+                //Tells player 1 to switch to player 2 once player moves
+                System.out.println("switch 2");
+            //if current player is player 2 switch to player 1 after player 2 reaches new square
+            } else if (currentPlayer == 2) {
+                currentPlayer = 1;
+                currentSpace2 = currentSpace;
+                //Tells player 2 to switch to player 1 once player moves
+                System.out.println("switch 1");
             }
         }
     }
